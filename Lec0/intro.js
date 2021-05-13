04c61d897a75b6b3b113032ef9d17db847462752
manthann3 <manthansharma35@gmail.com>
2021-05-13 19:35:51 +0530

    added comments

--- Lec0/intro.js
+++ Lec0/intro.js
@@ -1,3 +1,8 @@
 console.log("Ganesha!!!");
 
+//USING VSCODE WALA GITHUB
 // (1) git init :- makes the folder the repo of git 
+// (2) stage all the changes
+// (3) commit the changes 
+// (4) remote add origin
+// (5) push
